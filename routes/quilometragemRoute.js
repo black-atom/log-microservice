8314346287052af94130fceb73d7fb055b47cfbd
Felipe Francischetto <felipefrancischetto@hotmail.com>
2017-12-20 16:32:15 -0200

    fix: changing the way you call the route.

--- routes/quilometragemRoute.js
+++
@@ -1,9 +0,0 @@
-const quilometragemController = require('../controllers/quilometragemController')
-const route = require('express').Router()
-
-route.get('/quilometragens', quilometragemController.getAll)
-route.get('/quilometragens/:id', quilometragemController.getQuilometragemByID)
-route.post('/quilometragens', quilometragemController.SaveQuilometragem)
-route.put('/quilometragens/:id', quilometragemController.updateQuilometragem)
-
-module.exports = route
