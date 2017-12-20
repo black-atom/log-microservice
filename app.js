603a8dbc795161cb7da22f2828f82e29e1bf4a8e
Felipe Francischetto <felipefrancischetto@hotmail.com>
2017-12-20 16:25:57 -0200

    feature: adding new dependencie Cors.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const databaseConnection = require('./databaseConnection')
-const quilometragemRoute = require('./routes/quilometragemRoute')
+import express from 'express'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+import databaseConnection from './databaseConnection'
+import quilometragemRoute from './api/routes/quilometragemRoute'
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(bodyParser.text())
 app.use(bodyParser.json())
@@ -14,7 +16,6 @@ app.use(bodyParser.json({ type: 'application/json' }))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api', quilometragemRoute)
-
 app.listen(port, () => console.log('Running ...'))
 
-module.exports  = { app, port }
+export { app, port }
